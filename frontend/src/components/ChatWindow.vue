da4e52be7a8fea99ef81cfe050317e96d4b1169d
ChenLab <k4917552@gmail.com>
2025-08-02 19:15:31 +0800

    Add files via upload

---
+++ frontend/src/components/ChatWindow.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col flex-1 overflow-hidden">
+    <MessageList />
+    <MessageInput />
+  </div>
+</template>
+
+<script setup>
+import MessageList from './MessageList.vue';
+import MessageInput from './MessageInput.vue';
+</script>
